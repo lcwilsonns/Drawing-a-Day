40038f06686a3d20b8751ee177337e9373b9c285
Laura Wilson <lcwilson.ns@gmail.com>
2024-04-03 22:12:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//Word array and variables
+const words = ["Ocean", "Yellow", "Home", "Fly", "Bird", "Expanse", "Wave", "Read", "Open", "Closed", "Stone", "Wood", "Old", "New", "Lake", "Petal", "Forest", "Blue", "Green", "Poplar", "Evergreen", "Orange", "Metal", "Cover", "Chair"];
+const chosenWords = [];
+
+//Select a random word from the array. Array length = 25
+let randomNumber = Math.floor(Math.random()*(words.length-1));
+let randomChosenWord = words[randomNumber];
+chosenWords.push(randomChosenWord);
+console.log(chosenWords);
+
+document.querySelector(".btn").addEventListener("click", function () {
+    let randomNumber = Math.floor(Math.random()*(words.length-1));
+    let randomChosenWord = words[randomNumber];
+    document.querySelector("h3").innerHTML = randomChosenWord + "!";
+    chosenWords.push(randomChosenWord);
+})
+
+
