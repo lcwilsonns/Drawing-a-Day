55755b94f7a1a81952c35f83d5d38ccc51fcf64f
Laura Wilson <lcwilson.ns@gmail.com>
2024-04-03 22:41:11 -0300

    added to arrary

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 //Word array and variables
-const words = ["Ocean", "Yellow", "Home", "Fly", "Bird", "Expanse", "Wave", "Read", "Open", "Closed", "Stone", "Wood", "Old", "New", "Lake", "Petal", "Forest", "Blue", "Green", "Poplar", "Evergreen", "Orange", "Metal", "Cover", "Chair"];
+const words = [
+  "Ocean",
+  "Yellow",
+  "Home",
+  "Fly",
+  "Bird",
+  "Expanse",
+  "Wave",
+  "Read",
+  "Open",
+  "Closed",
+  "Stone",
+  "Wood",
+  "Old",
+  "New",
+  "Lake",
+  "Petal",
+  "Forest",
+  "Blue",
+  "Green",
+  "Poplar",
+  "Evergreen",
+  "Orange",
+  "Metal",
+  "Cover",
+  "Chair",
+  "Window",
+  "Door",
+  "Morning",
+  "Wheels",
+  "Mobile",
+];
 const chosenWords = [];
 
-//Select a random word from the array. Array length = 25
-let randomNumber = Math.floor(Math.random()*(words.length-1));
+//Select a random word from the array.
+let randomNumber = Math.floor(Math.random() * (words.length - 1));
 let randomChosenWord = words[randomNumber];
-chosenWords.push(randomChosenWord);
-console.log(chosenWords);
+// chosenWords.push(randomChosenWord);
+// console.log(chosenWords);
 
 document.querySelector(".btn").addEventListener("click", function () {
-    let randomNumber = Math.floor(Math.random()*(words.length-1));
-    let randomChosenWord = words[randomNumber];
-    document.querySelector("h3").innerHTML = randomChosenWord + "!";
-    chosenWords.push(randomChosenWord);
-})
-
-
+  let randomNumber = Math.floor(Math.random() * (words.length - 1));
+  let randomChosenWord = words[randomNumber];
+  document.querySelector("h3").innerHTML = randomChosenWord + "!";
+  chosenWords.push(randomChosenWord);
+});
